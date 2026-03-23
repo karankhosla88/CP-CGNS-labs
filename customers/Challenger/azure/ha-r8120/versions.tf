8be3359aba8324d8395ce26dcd65685cc8dbd27f
karankhosla88 <karan.khosla88@gmail.com>
2026-03-23 16:49:15 +1100

    Create versions.tf

---
+++ customers/Challenger/azure/ha-r8120/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
