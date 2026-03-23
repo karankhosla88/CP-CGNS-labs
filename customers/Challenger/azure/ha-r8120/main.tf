efd19aee95a80b966987298fb68da0523a1150a2
karankhosla88 <karan.khosla88@gmail.com>
2026-03-23 16:46:42 +1100

    Create main.tf

---
+++ customers/Challenger/azure/ha-r8120/main.tf
@@ -0,0 +1,28 @@
+module "cloudguard_ha" {
+  source = "git::https://github.com/karankhosla88/terraform-azure-cloudguard-network-security.git//modules/high-availability?ref=v1.2.1-patched2"
+
+  subscription_id      = var.subscription_id
+  tenant_id            = var.tenant_id
+  client_id            = var.client_id
+  client_secret        = var.client_secret
+  resource_group_name  = var.resource_group_name
+  cluster_name         = var.cluster_name
+  location             = var.location
+  availability_type = var.availability_type
+  authentication_type  = var.authentication_type
+  admin_password       = var.admin_password
+  sic_key              = var.sic_key
+  vm_size              = var.vm_size
+  disk_size            = var.disk_size
+  os_version           = var.os_version
+  vm_os_sku            = var.vm_os_sku
+  vm_os_offer          = var.vm_os_offer
+  allow_upload_download = var.allow_upload_download
+  vnet_name            = var.vnet_name
+  frontend_subnet_name = var.frontend_subnet_name
+  backend_subnet_name  = var.backend_subnet_name
+  smart_1_cloud_token_a            = var.smart_1_cloud_token_a
+  smart_1_cloud_token_b            = var.smart_1_cloud_token_b
+  serial_console_password_hash     = var.serial_console_password_hash
+  maintenance_mode_password_hash   = var.maintenance_mode_password_hash
+}
