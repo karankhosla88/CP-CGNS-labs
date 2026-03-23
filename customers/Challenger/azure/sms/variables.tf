a12b2d4a3889d1cfe486dae7cbcaeafe1d362a19
karankhosla88 <karan.khosla88@gmail.com>
2026-03-24 09:12:16 +1100

    Update variables.tf

--- customers/Challenger/azure/sms/variables.tf
+++ customers/Challenger/azure/sms/variables.tf
@@ -1,4 +1,9 @@
-variable "subscription_id" {
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_id" {
   type = string
 }
 
@@ -6,25 +11,30 @@ variable "tenant_id" {
   type = string
 }
 
-variable "client_id" {
+variable "subscription_id" {
   type = string
-}
-
-variable "client_secret" {
-  type      = string
-  sensitive = true
 }
 
 variable "resource_group_name" {
   type = string
 }
 
-variable "cluster_name" {
+variable "mgmt_name" {
   type = string
 }
 
 variable "location" {
   type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "source_image_vhd_uri" {
+  type    = string
+  default = "noCustomUri"
 }
 
 variable "authentication_type" {
@@ -36,7 +46,12 @@ variable "admin_password" {
   sensitive = true
 }
 
-variable "sic_key" {
+variable "serial_console_password_hash" {
+  type      = string
+  sensitive = true
+}
+
+variable "maintenance_mode_password_hash" {
   type      = string
   sensitive = true
 }
@@ -65,15 +80,22 @@ variable "allow_upload_download" {
   type = bool
 }
 
+variable "admin_shell" {
+  type    = string
+  default = "/etc/cli.sh"
+}
+
+variable "bootstrap_script" {
+  type    = string
+  default = ""
+}
+
+variable "zone" {
+  type    = string
+  default = ""
+}
+
 variable "vnet_name" {
-  type = string
-}
-
-variable "frontend_subnet_name" {
-  type = string
-}
-
-variable "backend_subnet_name" {
   type = string
 }
 
@@ -81,26 +103,36 @@ variable "subnet_name" {
   type = string
 }
 
-variable "smart_1_cloud_token_a" {
-  type      = string
-  sensitive = true
-}
-
-variable "smart_1_cloud_token_b" {
-  type      = string
-  sensitive = true
-}
-
-variable "serial_console_password_hash" {
-  type      = string
-  sensitive = true
-}
-
-variable "maintenance_mode_password_hash" {
-  type      = string
-  sensitive = true
-}
-
-variable "availability_type" {
+variable "address_space" {
   type = string
+}
+
+variable "subnet_prefix" {
+  type = string
+}
+
+variable "management_GUI_client_network" {
+  type = string
+}
+
+variable "mgmt_enable_api" {
+  type = string
+}
+
+variable "nsg_id" {
+  type    = string
+  default = ""
+}
+
+variable "storage_account_deployment_mode" {
+  type = string
+}
+
+variable "add_storage_account_ip_rules" {
+  type = bool
+}
+
+variable "storage_account_additional_ips" {
+  type    = list(string)
+  default = []
 }
